3c37848410190e4bb309fe3e68527892d958912c
Idanturjeman <idan_turjeman29@walla.co.il>
2022-06-04 13:42:14 +0300

    change reactor

--- reactor.cpp
+++ reactor.cpp
@@ -1,35 +1,29 @@
 
 #include "reactor.hpp"
 
-// typedef struct reactor
-// {
-//     int fd;
-//     pthread_t id;
-//     void*(*func)(void*);
-// }reactor,*preactor;
-
-// typedef struct reqests{
-//     int fd;
-//     preactor p_reactor;
-// }reqests, *preqests;
+static int numOfHandler = 0;
 
 
 preactor newReactor(){
+    int rec = 0;
     preactor r = (preactor)(malloc(sizeof(reactor)));
     return r;
 }
-
 void InstallHandler(preactor p_reactor, void*(func)(void*), int file_des){
+    numOfHandler++;
     p_reactor->func = func;
     p_reactor->fd = file_des;
     preqests p_reqests = (preqests)(malloc(sizeof(reqests)));
     p_reqests->fd = file_des;
+    int inHand = 0;
     p_reqests->p_reactor = p_reactor;
     pthread_create(&p_reactor->id, NULL, func, p_reqests);
 }
 void RemoveHandler(preactor p_reactor, int fd_free){
+numOfHandler--;
 pthread_join(p_reactor->id, NULL);
 p_reactor->fd = -1;
+int reHand = 0;
 p_reactor->func = NULL;
 }
 
