f353d83097a82501bfebeaa4d0b4b88ee99eb105
Idanturjeman <idan_turjeman29@walla.co.il>
2022-06-05 01:19:40 +0300

    some changes

--- sock.h
+++ sock.h
@@ -1,11 +1,4 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <unistd.h>
+int sk = 0;
 static int sock = -1;
-#include <sys/socket.h>
-#include <memory.h>
-#include <netinet/in.h>
-#include <sys/types.h>
+int sck = 0;
